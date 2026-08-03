aa4b149777231f2d2b41542d9894c3a5341bf479
t2t2t2t2t <takijiograce@gmail.com>
2026-08-03 12:07:30 +0200

    new commit

--- js/pricing.js
+++ js/pricing.js
@@ -1,21 +1,66 @@
-const t = "bonjour";
-console.log(t);
-const a= "grace";
-const b=20;
-console.log(`bonjour je mappelle ${a} jai ${b}ans`);
-const T=28;
-const G=2;
-const c=T+G;
-console.log(c);
-console.log(`la somme est ${T+G} `);
-const pT=T*c;
-const pb=G*c;
-if (pT>40) {
+// const t = "bonjour";
+// console.log(t);
+// const a= "grace";
+// const b=20;
+// console.log(`bonjour je mappelle ${a} jai ${b}ans`);
+// const T=28;
+// const G=2;
+// const c=T+G;
+// console.log(c);
+// console.log(`la somme est ${T+G} `);
+// const pT=T*c;
+// const pb=G*c;
+// if (pT>40) {
     
  
+// }
+// if (T!==G){
+//     console.log(T);
+// }
+// let d =20;
+// let f=10;
+const n = [ "bijoel", "grace","serena"]
+for( let i=0; i<n.length; i++ ) {
+    console.log (n[i])
+
 }
-if (T!==G){
-    console.log(T);
+const R ={admis :"ADMIS",
+    refuse:"REFUSE"
+
 }
-let d =20;
-let f=10;
+
+switch (R.admis){
+    case "ADMIS" : console.log("T");
+    break;
+    case "REFUSE" : console.log("D");
+    break;
+};
+const s=7;
+
+switch(s){
+    case 5: console.log(`${s}`);
+    break
+    case 7 : console.log(`${s}`);
+    break;
+    
+    case 4 : console.log(`${s}`);
+    break;
+}
+
+let i=1;
+while (i<=5){
+console.log(i);
+i++;
+}
+function divisibilite (n){
+    if (n %3 === 0){
+        return console.log("is a multiple 3")
+     }
+     return console.log("is not");
+}
+
+
+
+divisibilite(9);
+
+
